75ed80b961e7440d72730bcf3f246d5d57b33c9d
akihiro190e <nishikawa_ssd@yahoo.co.jp>
2019-01-06 17:58:19 +0900

    modified migration

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20190106055441) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "sns_uname"
+    t.string   "name"
     t.string   "provider"
     t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
